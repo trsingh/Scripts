8b7ede15692564fc40fd6f592f2b786542b29652
trsingh <tejpratap.singh@finzly.com>
2025-08-19 09:12:16 -0400

    udpate

--- DevOps/DevOps.sql
+++ DevOps/DevOps.sql
@@ -61,7 +61,16 @@ select
 	ds.build_number,
 	ds.deployment_datetime ,
 	ds.updated_by ,
-	ds.updated_datetime
+	ds.updated_datetime,
+	CASE 
+		when ds.env_cd like 'DEV%' then 1
+		when ds.env_cd like 'QA%' then 2
+		when ds.env_cd like 'TEST%' then 3
+		when ds.env_cd like 'UAT%' then 4
+		when ds.env_cd like 'PFIX%' then 5
+		when ds.env_cd like 'PROD%' then 6
+		else 10
+	END as rank_col
 from
 	DEPLOYMENT_SNAPSHOT ds
 inner join (
@@ -79,18 +88,30 @@ inner join (
 		and ds.comp_cd = ds2.comp_cd
 		and ds.deployment_datetime = ds2.latest_deployment_datetime)
 WHERE 
-	YEAR(ds.deployment_datetime) = 2025
-	and MONTH(ds.deployment_datetime) >= 07
--- 	and (ds.env_cd like 'QA%' or ds.env_cd like '%DEV%' or ds.env_cd like '%TEST%')
-	and (ds.env_cd like 'UAT%' or ds.env_cd like '%PFIX%' or ds.env_cd like '%PROD%')
--- 	and ds.comp_cd like '%compliance%'
+	YEAR(ds.deployment_datetime) >= 2025
+	and MONTH(ds.deployment_datetime) >= 01
+	and not (ds.env_cd like 'UAT%'
+		or ds.env_cd like '%PFIX%'
+		or ds.env_cd like '%PROD%')
+	-- 	and (ds.env_cd like 'UAT%' or ds.env_cd like '%PFIX%' or ds.env_cd like '%PROD%')
+	and ds.comp_cd like '%teller%'
 	-- 	and em.env_name like 'PROD%'
--- 	and ds.env_cd = 'DEV'
+	-- 	and ds.env_cd like 'PFIX%'
+	-- 	and ds.env_cd = 'DEV'
 order by
-	ds.env_cd ,
+	rank_col ,
+	ds.env_cd,
 	ds.comp_cd,
 	ds.deployment_datetime desc;
 
+
+select
+	*
+from
+	DEPLOYMENT_SNAPSHOT ds
+where
+	ds.comp_cd like '%nostro%'
+	and ds.env_cd like 'DEV%'
 
 
 
